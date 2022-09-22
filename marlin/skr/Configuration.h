0e7c4ecb422100db26f3af94b3c9b954d83e7f8c
Oleksandr Minenko <oleksandr.minenko@ringteam.com>
2022-09-22 14:38:26 +0300

    upd extuder

--- marlin/skr/Configuration.h
+++ marlin/skr/Configuration.h
@@ -1029,7 +1029,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 480, 480, 1600, 194 } // { 160, 160, 1600, 194 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 480, 480, 1600, 578 } // { 160, 160, 1600, 194 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
@@ -1465,7 +1465,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
